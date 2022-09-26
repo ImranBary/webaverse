68e128e9a1bd44324e6a478e199007deed115b6a
Imran <100944041+ImranBary@users.noreply.github.com>
2022-09-26 12:24:30 +0100

    Create flicker.js

---
+++ audio/flicker.js
@@ -0,0 +1,34 @@
+import * as THREE from "three";
+import metaversefile from "metaversefile";
+const { useApp, useCamera, useCleanup } = metaversefile;
+
+const baseUrl = import.meta.url.replace(/(\/)[^\/\\]*$/, "$1");
+
+export default () => {
+  const app = useApp();
+  const camera = useCamera();
+
+  const listener = new THREE.AudioListener();
+  camera.add(listener);
+
+  // create a global audio source
+  const sound = new THREE.Audio(listener);
+
+  // load a sound and set it as the Audio object's buffer
+  const audioLoader = new THREE.AudioLoader();
+  audioLoader.load(
+    baseUrl + "/sounds/Dying Light Bulb-SoundBible.com-742005847.mp3",
+    function (buffer) {
+      sound.setBuffer(buffer);
+      sound.setLoop(true);
+      sound.setVolume(0.25);
+      sound.play();
+    }
+  );
+
+  useCleanup(() => {
+    sound.stop();
+  });
+
+  return app;
+};
