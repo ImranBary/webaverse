8f1f26dc00bdce87a848d3e67b5340c07faaaa2e
ImranBary <imranfeisalAB@gmail.com>
2022-09-26 10:48:32 +0100

    and then there was sound

---
+++ audio/spooky.js
@@ -0,0 +1,34 @@
+import * as THREE from "three";
+import metaversefile from "metaversefile";
+const { useApp, useCamera, useCleanup } = metaversefile;
+
+const baseUrl = import.meta.url.replace(/(\/)[^\/\\]*$/, "$1");
+
+export default () => {
+  const app = useApp();
+  const camera = useCamera();
+
+  const listener = new THREE.AudioListener();
+  camera.add(listener);
+
+  // create a global audio source
+  const sound = new THREE.Audio(listener);
+
+  // load a sound and set it as the Audio object's buffer
+  const audioLoader = new THREE.AudioLoader();
+  audioLoader.load(
+    baseUrl + "/sounds/Surreal Metal Clangs - QuickSounds.com.mp3",
+    function (buffer) {
+      sound.setBuffer(buffer);
+      sound.setLoop(true);
+      sound.setVolume(0.25);
+      sound.play();
+    }
+  );
+
+  useCleanup(() => {
+    sound.stop();
+  });
+
+  return app;
+};
